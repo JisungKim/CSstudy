02eca9c27add1d8f39319a6b2ce0e638159ba357
Jisung Kim <kimjs3115@gmail.com>
2019-03-10 21:56:04 +0900

    3/10 스터디 마무리

--- Django/0_Official-tutorial/mysite/polls/views.py
+++ Django/0_Official-tutorial/mysite/polls/views.py
@@ -4,6 +4,23 @@ from django.template import loader
 from .models import Question, Choice
 from django.http import Http404
 from django.urls import reverse
+from django.shortcuts import get_object_or_404, render
+from django.views import generic
+
+class IndexView(generic.ListView):
+	template_name = "polls/index.html"
+	context_object_name = "latest_question_list"
+
+	def get_queryset(self):
+		return Question.objects.order_by("-pub_date")[:5]
+
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
 
 def index(request):
 
